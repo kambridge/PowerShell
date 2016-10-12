788e54984c20ce5fee1e9e5e78ebe8b3a8904051
kambridge <kambridge.giles@gmail.com>
2016-10-12 11:00:37 -0400

    Create Set.PsISEcolorsAndFont.ps1

---
+++ Styles/ISEProfile/KGDocuments/PowerShellISE/Scripts/autoload/Set.PsISEcolorsAndFont.ps1
@@ -0,0 +1,37 @@
+#Change PSISE
+# fonts
+#$psISE.Options.FontName 
+#$psISE.Options.FontName = 'Kartika' 
+$psISE.Options.FontSize = 12 
+# output pane 
+$psISE.Options.OutputPaneBackgroundColor = '#2E3133' 
+$psISE.Options.OutputPaneTextBackgroundColor = '#2E3133' 
+$psISE.Options.OutputPaneForegroundColor = '#FFFFFF' 
+# command pane 
+$psISE.Options.CommandPaneBackgroundColor = '#2E3133' 
+# script pane 
+$psISE.Options.ScriptPaneBackgroundColor = '#2E3133' 
+# tokens 
+$psISE.Options.TokenColors.item('Command') = '#66D9EF'  
+$psISE.Options.TokenColors.item('Operator') = '#FFFFFF' 
+$psISE.Options.TokenColors.item('Unknown') = '#FF8C00' 
+$psISE.Options.TokenColors.item('Member') = '#E6DB74' 
+$psISE.Options.TokenColors.item('Position') = '#00598C' 
+$psISE.Options.TokenColors.item('GroupEnd') = '#808080' 
+$psISE.Options.TokenColors.item('GroupStart') = '#808080' 
+$psISE.Options.TokenColors.item('LineContinuation') = '#F8F8F2' 
+$psISE.Options.TokenColors.item('NewLine') = '#00FFFF' 
+$psISE.Options.TokenColors.item('StatementSeparator') = '#FFFFFF' 
+$psISE.Options.TokenColors.item('Comment') = '#808080' 
+$psISE.Options.TokenColors.item('String') = '#AE81FF' 
+$psISE.Options.TokenColors.item('Keyword') = '#6E715E' 
+$psISE.Options.TokenColors.item('Attribute') = '#AEC5FF' 
+$psISE.Options.TokenColors.item('Type') = '#F92659' 
+$psISE.Options.TokenColors.item('Variable') = '#FD971F' 
+$psISE.Options.TokenColors.item('CommandParameter') = '#FFFFFF' 
+$psISE.Options.TokenColors.item('CommandArgument') = '#A6E22E' 
+$psISE.Options.TokenColors.item('Number') = '#00FFFF' 
+$psISE.Options.TokenColors.item('LoopLabel') = '#FD971F'
+
+#Default Configuration
+#s$psISE.Options.RestoreDefaults()
