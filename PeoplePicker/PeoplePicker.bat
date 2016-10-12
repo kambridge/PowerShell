b02d5f2658cca5403affe12948eac6fb6501a1ba
kambridge <kambridge.giles@gmail.com>
2016-10-12 08:24:04 -0400

    Update PeoplePicker.bat

--- PeoplePicker/PeoplePicker.bat
+++ PeoplePicker/PeoplePicker.bat
@@ -1,13 +1,20 @@
-#People picker not pulling results
-#STSADM
-#Must run the setproperty command on every web application.
+#Error: Cannot retrieve the information for application credential key
+#Description: People picker not pulling results
+#Console: STSADM
+#Notes: Must run the setproperty command on every web application.
+#GetProperty *uses server:port
+## stsadm.exe -o getproperty -propertyname peoplepicker -searchadforests -url <servername:port>
+#SetPropery
+##stsadm -o setapppassword -password <password>
+##stsadm -osetproperty -pn peoplepicker-searchadforests -pv "<forest>,<IFPSERV account>" -url <servername:port>
+#Reference: https://technet.microsoft.com/en-us/library/cc263460(v=office.12).aspx
 
 cls
 echo========
 cd %commonprogramfiles%\microsoft shared\web server extension\14\bin
 @echo off
 
-stsadm -o setapppassword [password]
-stsadm -o setproperty -pn peoplepicker -searchadforests -pv "[forest],[IFPSERV account]" -url
+stsadm -o setapppassword <password>
+stsadm -o setproperty -pn peoplepicker-searchadforests -pv "<forest>,<IFPSERV account>" -url <servername:port>
 
 echo complete
