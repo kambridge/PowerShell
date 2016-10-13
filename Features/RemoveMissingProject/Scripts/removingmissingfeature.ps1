1e339bb4647fc0409d56a36f7fcf580bdb27bf0f
kambridge <kambridge.giles@gmail.com>
2016-10-13 12:05:40 -0400

    Update removingmissingfeature.ps1

--- Features/RemoveMissingProject/Scripts/removingmissingfeature.ps1
+++ Features/RemoveMissingProject/Scripts/removingmissingfeature.ps1
@@ -1,3 +1,10 @@
+<#
+[MissingFeature] Database [SharePoint_Content_Portal] has reference(s) to a missing feature: Id = [8096285f-1463-42c7-82b7-f745e5bacf29], 
+Name = [My Feature], Description = [], Install Location = [Test-MyFeature]. 
+The feature with Id 8096285f-1463-42c7-82b7-f745e5bacf29 is referenced in the database [SharePoint_Content_Portal], but is not installed on the current farm. 
+The missing feature may cause upgrade to fail. Please install any solution which contains the feature and restart upgrade if necessary.
+#>
+
 <#    NOTES: Removes feature from all site collections and sites    #>
 
 function Remove-SPFeatureFromContentDB($ContentDb, $FeatureId, [switch]$ReportOnly)
