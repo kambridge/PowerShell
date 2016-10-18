eacdff41487866751d3211ab5b97e96dd7a0ae2f
kambridge <kambridge.giles@gmail.com>
2016-10-18 14:52:09 -0400

    Create iisresetfarm.ps1

---
+++ SPServices/iisresetfarm.ps1
@@ -0,0 +1,24 @@
+#IIS RESET FOR ALL SP FARM SERVERS
+#DESCRIPTION:
+#NOTE:
+
+cls
+
+Write-Host "Loading SharePoint Commandlets" 
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+Write-Host -Foregroundcolor green "Commandlets loaded...Loading variables"
+
+Write-Host
+[array]$servers = Get-SPServer | ? {$_.Role -eq "Application"}
+$farm = Get-SPFarm
+
+foreach ($server in $servers){
+  Write-Host -ForegroundColor yello "Attempting to reset IIS for $server"
+  iisreset $server /noforce "\\"$_.Address
+  iisreset $server /Status "\\"$_.Address
+  Write-Host
+  Write-Host -Foregroundcolor green "IIS has been reset for $server"
+}
+
+Write-Host -Foregroundcolor green "IIS has been reset across SharePoint farm"
+Start-Sleep -Seconds 5
