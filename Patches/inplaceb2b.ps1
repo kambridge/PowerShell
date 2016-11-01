f82c3e321ff17cb386efa5151ff00576453f40ab
kambridge <kambridge.giles@gmail.com>
2016-11-01 09:02:11 -0400

    Update inplaceb2b.ps1

--- Patches/inplaceb2b.ps1
+++ Patches/inplaceb2b.ps1
@@ -1,5 +1,6 @@
 #PSConfig
 #Description: Open cmd as Administrator and run below command after installing Sharepoint Updates/paches. 
+#Check CA for servers that need to be upgraded: CA > System Settings > Manage servers in the farm > Upgrade Server
 #Notes: Run one server at a time.
 
 
